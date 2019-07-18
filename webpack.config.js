88984f8f4d515835f40ed71019860d37550cce59
walt2016 <22802768@qq.com>
2019-07-18 16:03:52 +0800

    'autocommit'

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 module.exports = {
     mode: "production",
-    // devtool: 'eval-source-map',
+    devtool: 'eval-source-map',
     entry: path.resolve(__dirname, "./app/index.js"), //唯一的入口文件
     output: {
         path: path.resolve(__dirname, "./dist"), //打包后的文件存放的地方
