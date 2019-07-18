2f408a8250aa3cf35c0b20bd83f8ab93d77e88d8
walt2016 <22802768@qq.com>
2019-07-18 11:20:18 +0800

    'autocommit'

--- build/push.js
+++ build/push.js
@@ -1,4 +1,10 @@
 var shell = require('shelljs');
+//检查控制台是否以运行`git `开头的命令
+if (!shell.which('git')) {
+    //在控制台输出内容
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+  }
 
 // if (shell.exec('npm run build').code !== 0) {
 //   shell.echo('Error: Git commit failed');
